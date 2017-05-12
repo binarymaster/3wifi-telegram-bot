24086d34042f8902d711b9a8d4472a19b3ee34a4
Ivan <leoheartff1@gmail.com>
2017-05-12 19:20:33 +0300

    http changed to https

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def handler(message):
         if tmp[0] == '/help': bot.send_message(message.chat.id, "3wifi.stascorp.com бот!\n/pw bssid/essid - поиск пароля и wps пина по мак адресу или имени точки (пример: /pw FF:FF:FF:FF:FF:FF или /pw netgear)\nwps /bssid - поиск wps пина по мак адресу (пример: /wps FF:FF:FF:FF:FF:FF)")
         elif len(tmp) == 2:
             if tmp[0] == '/pw' and re.match(bssid_pattern, tmp[1]) != None:
-                r = requests.get('http://3wifi.stascorp.com/api/apiquery?key={}&bssid={}'.format(API_key, tmp[1]))
+                r = requests.get('https://3wifi.stascorp.com/api/apiquery?key={}&bssid={}'.format(API_key, tmp[1]))
                 result = r.json()
                 if len(result['data']) > 0:
                     result = result['data'][tmp[1].upper()]
@@ -37,7 +37,7 @@ def handler(message):
                         else: bot.send_message(message.chat.id, '<нет WPS пина>')
                 else: bot.send_message(message.chat.id, 'Нет результатов :(')
             elif re.match(bssid_pattern, tmp[1]) == None:
-                r = requests.get('http://3wifi.stascorp.com/api/apiquery?key={}&bssid=*&essid={}'.format(API_key, tmp[1]))
+                r = requests.get('https://3wifi.stascorp.com/api/apiquery?key={}&bssid=*&essid={}'.format(API_key, tmp[1]))
                 result = r.json()
                 if len(result['data']) > 0:
                     result = result['data']['*|{}'.format(tmp[1])]
@@ -51,7 +51,7 @@ def handler(message):
                         else: bot.send_message(message.chat.id, '<нет WPS пина>')
                 else: bot.send_message(message.chat.id, 'Нет результатов :(')
             elif tmp[0] == '/wps':
-                r = requests.get('http://3wifi.stascorp.com/api/apiwps?key={}&bssid={}'.format(API_key, tmp[1]))
+                r = requests.get('https://3wifi.stascorp.com/api/apiwps?key={}&bssid={}'.format(API_key, tmp[1]))
                 result = r.json()
                 if len(result['data']) > 0:
                     result = result['data'][tmp[1].upper()]['scores']
@@ -60,13 +60,13 @@ def handler(message):
                         bot.send_message(message.chat.id, value['value'])
                         bot.send_message(message.chat.id, 'Score: ' + str(value['score']))
                 else: bot.send_message(message.chat.id, 'Нет результатов :(')
-        else: bot.send_message(message.chat.id, 'Команда не найдена :( Отправь "help" для получения списка команд')
+        else: bot.send_message(message.chat.id, 'Команда не найдена :( Отправь "/help" для получения списка команд')
     except: bot.send_message(message.chat.id, 'Произошла ошибка :( Жди, когда 3wifi заработает. Если же он работает - отпиши автору о баге')
 
 
 while API_key == '':
     try:
-        r = requests.post('http://3wifi.stascorp.com/api/apikeys', data = {'login':'antichat', 'password':'antichat'})
+        r = requests.post('https://3wifi.stascorp.com/api/apikeys', data = {'login':'antichat', 'password':'antichat'})
         result = r.json()
         if result['result']: API_key = result['data'][0]['key']
     except: print('Problem with 3wifi')
