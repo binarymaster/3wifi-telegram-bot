3073fddf89cb67c78918c55a5b83a86c6f7ee2b2
Victor <drygdryg2014@yandex.ru>
2020-01-09 23:53:09 +0300

    Refactor according to PEP8

--- bot.py
+++ bot.py
@@ -6,24 +6,27 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 TOKEN = ''
-try: TOKEN = open('apikey.txt').read()
-except:
+try:
+    TOKEN = open('apikey.txt').read()
+except FileNotFoundError:
     TOKEN = input('telegram token: ')
     outf = open('apikey.txt', 'w')
     outf.write(TOKEN)
     outf.close()
 
 IP = ''
-try: IP = open('IP.txt').read()
-except:
+try:
+    IP = open('IP.txt').read()
+except FileNotFoundError:
     IP = input('IP for webhook: ')
     outf = open('IP.txt', 'w')
     outf.write(IP)
     outf.close()
 
 API_KEY = ''
-try: API_KEY = open('3wifiapi.txt').read()
-except:
+try:
+    API_KEY = open('3wifiapi.txt').read()
+except FileNotFoundError:
     API_KEY = input('3WiFi API read key: ')
     outf = open('3wifiapi.txt', 'w')
     outf.write(API_KEY)
@@ -31,17 +34,22 @@ except:
 
 bssid_pattern = re.compile(r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$")
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def text(bot, update): update.message.reply_text('Команда не найдена! Отправьте /help для получения информации по доступным командам')
+def text(bot, update):
+    update.message.reply_text('Команда не найдена! Отправьте /help для получения информации по доступным командам')
+
 
 def help(bot, update):
     update.message.reply_text(f'''3wifi.stascorp.com бот!
 /pw bssid и/или essid - поиск по мак адресу или имени точки (пример: /pw FF:FF:FF:FF:FF:FF или /pw netgear или /pw FF:FF:FF:FF:FF:FF VILTEL)
 /pws - /pw, но с учётом регистра (essid)
 /wps bssid - поиск wps пина по мак адресу (пример: /wps FF:FF:FF:FF:FF:FF)''')
+
 
 def printap(value):
     answer = f"""ESSID: `{value['essid']}`
@@ -56,11 +64,13 @@ Time: {value['time']}
         answer += '- - - - -\n'
     return answer
 
+
 def printaps(values):
     answer = ''
     for value in values:
         answer += printap(value)
     return answer
+
 
 def CheckAPresponse(data):
     if data['result'] == 0:
@@ -72,11 +82,12 @@ def CheckAPresponse(data):
         return 'Нет результатов :('
     return ''
 
+
 def pw(bot, update):
-    answer='Забыли ввести bssid или essid! Поиск по bssid и/или essid выполняется так: /pw bssid/essid (пример: /pw FF:FF:FF:FF:FF:FF VILTEL или /pw FF:FF:FF:FF:FF:FF или /pw netgear)'
+    answer = 'Забыли ввести bssid или essid! Поиск по bssid и/или essid выполняется так: /pw bssid/essid (пример: /pw FF:FF:FF:FF:FF:FF VILTEL или /pw FF:FF:FF:FF:FF:FF или /pw netgear)'
     tmp = update.message.text.split()
     if len(tmp) == 2:
-        answer=''
+        answer = ''
         if re.match(bssid_pattern, tmp[1]) is not None:
             results = requests.get(f'https://3wifi.stascorp.com/api/apiquery?key={API_KEY}&bssid={tmp[1]}').json()
             answer = CheckAPresponse(results)
@@ -89,18 +100,20 @@ def pw(bot, update):
                 answer = printaps(results['data'][f'*|{tmp[1]}'])
     elif len(tmp) == 3:
         if re.match(bssid_pattern, tmp[1]) is not None:
-            results = requests.get(f'https://3wifi.stascorp.com/api/apiquery?key={API_KEY}&bssid={tmp[1]}&essid={tmp[2]}').json()    
+            results = requests.get(f'https://3wifi.stascorp.com/api/apiquery?key={API_KEY}&bssid={tmp[1]}&essid={tmp[2]}').json()
             answer = CheckAPresponse(results)
             if answer == '' and len(results['data']) == 1:
                 answer = printap(results['data'][f'{tmp[1].upper()}|{tmp[2]}'][0])
-    else: answer='Поиск по bssid и essid выполняется так: /pw bssid essid (пример: /pw FF:FF:FF:FF:FF:FF VILTEL)'
+    else:
+        answer = 'Поиск по bssid и essid выполняется так: /pw bssid essid (пример: /pw FF:FF:FF:FF:FF:FF VILTEL)'
     update.message.reply_text(answer, parse_mode='Markdown')
 
+
 def pws(bot, update):
-    answer='Забыли ввести bssid или essid! Поиск по bssid и/или essid выполняется так: /pws bssid/essid (пример: /pws FF:FF:FF:FF:FF:FF VILTEL или /pws FF:FF:FF:FF:FF:FF или /pws netgear)'
+    answer = 'Забыли ввести bssid или essid! Поиск по bssid и/или essid выполняется так: /pws bssid/essid (пример: /pws FF:FF:FF:FF:FF:FF VILTEL или /pws FF:FF:FF:FF:FF:FF или /pws netgear)'
     tmp = update.message.text.split()
     if len(tmp) == 2:
-        answer=''
+        answer = ''
         if re.match(bssid_pattern, tmp[1]) is not None:
             results = requests.get(f'https://3wifi.stascorp.com/api/apiquery?key={API_KEY}&bssid={tmp[1]}').json()
             answer = CheckAPresponse(results)
@@ -113,21 +126,23 @@ def pws(bot, update):
                 answer = printaps(results['data'][f'*|{tmp[1]}'])
     elif len(tmp) == 3:
         if re.match(bssid_pattern, tmp[1]) is not None:
-            results = requests.get(f'https://3wifi.stascorp.com/api/apiquery?key={API_KEY}&bssid={tmp[1]}&essid={tmp[2]}&sens=true').json()    
+            results = requests.get(f'https://3wifi.stascorp.com/api/apiquery?key={API_KEY}&bssid={tmp[1]}&essid={tmp[2]}&sens=true').json()
             answer = CheckAPresponse(results)
             if answer == '' and len(results['data']) == 1:
                 answer = printap(results['data'][f'{tmp[1].upper()}|{tmp[2]}'][0])
-    else: answer='Поиск по bssid и essid выполняется так: /pws bssid essid (пример: /pws FF:FF:FF:FF:FF:FF VILTEL)'
+    else:
+        answer = 'Поиск по bssid и essid выполняется так: /pws bssid essid (пример: /pws FF:FF:FF:FF:FF:FF VILTEL)'
     update.message.reply_text(answer, parse_mode='Markdown')
 
+
 def wps(bot, update):
-    answer='Поиск wps пин кодов выполняется так: /wps bssid (пример: /wps FF:FF:FF:FF:FF:FF)'
+    answer = 'Поиск wps пин кодов выполняется так: /wps bssid (пример: /wps FF:FF:FF:FF:FF:FF)'
     tmp = update.message.text.split()
     if len(tmp) == 2:
         if re.match(bssid_pattern, tmp[1]) is not None:
             results = requests.get('https://3wifi.stascorp.com/api/apiwps?key={}&bssid={}'.format(API_KEY, tmp[1])).json()
             if len(results['data']) > 0:
-                answer=''
+                answer = ''
                 for result in results['data'][tmp[1].upper()]['scores']:
                     result['score'] *= 100
                     if result['score'] < 1:
@@ -136,18 +151,21 @@ def wps(bot, update):
                         score = "{0:.1f}".format(result['score'])
                     else:
                         score = str(round(result['score']))
-                    answer+=f"""Name: `{result['name']}`
+                    answer += f"""Name: `{result['name']}`
 Pin: `{result['value']}`
 Score: {score}%
 - - - - -
 """
-            else: answer = 'Нет результатов :('
-    if len(answer) > 3900: update.message.reply_text(answer[:3900] + '\nСписок слишком большой, смотри дальше на 3wifi.stascorp.com', parse_mode='Markdown')
-    else: update.message.reply_text(answer, parse_mode='Markdown')
+            else:
+                answer = 'Нет результатов :('
+    if len(answer) > 3900:
+        update.message.reply_text(answer[:3900] + '\nСписок слишком большой, смотри дальше на 3wifi.stascorp.com', parse_mode='Markdown')
+    else:
+        update.message.reply_text(answer, parse_mode='Markdown')
+
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
-
 
 
 updater = Updater(TOKEN)
@@ -159,5 +177,7 @@ dp.add_handler(CommandHandler("pw", pw))
 dp.add_handler(CommandHandler("pws", pws))
 dp.add_handler(MessageHandler(Filters.text, text))
 dp.add_error_handler(error)
-if IP == 'no': updater.start_polling(poll_interval=.5)
-else: updater.start_webhook(listen='0.0.0.0', port=8443, url_path=TOKEN, key='private.key', cert='cert.pem', webhook_url=f'https://{IP}:8443/{TOKEN}')
+if IP == 'no':
+    updater.start_polling(poll_interval=.5)
+else:
+    updater.start_webhook(listen='0.0.0.0', port=8443, url_path=TOKEN, key='private.key', cert='cert.pem', webhook_url=f'https://{IP}:8443/{TOKEN}')
